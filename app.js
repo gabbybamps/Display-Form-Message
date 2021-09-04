85748112c82f7dab3c6b2fa3723613d28dbb5e9f
gabbybamps <gabbybamps@gmail.com>
2021-09-04 22:01:44 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+// (function () {
+//   const form = document.querySelector("#message-form");
+
+//   form.addEventListener("submit", function (e) {
+//     e.preventDefault();
+
+//     const message = document.querySelector(`#message`);
+//     const feedback = document.querySelector(`.feedback`);
+//     const messageContent = document.querySelector(`.message-content`);
+
+//     // if (!message.value) return;
+
+//     if (message.value === "") {
+//       feedback.classList.add("show");
+//       setTimeout(function () {
+//         feedback.classList.remove("show");
+//       }, 5000);
+//     } else {
+//       messageContent.textContent = message.value;
+//       message.value = "";
+//     }
+//   });
+// })();
+
+const submitForm = function () {
+  const form = document.querySelector("#message-form");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const message = document.querySelector(`#message`);
+    const feedback = document.querySelector(`.feedback`);
+    const messageContent = document.querySelector(`.message-content`);
+
+    // if (!message.value) return;
+
+    if (message.value === "") {
+      feedback.classList.add("show");
+      setTimeout(function () {
+        feedback.classList.remove("show");
+      }, 5000);
+    } else {
+      messageContent.textContent = message.value;
+      message.value = "";
+    }
+  });
+};
+
+submitForm();
